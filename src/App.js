5633c0be709741022c0c7a57830645e2ba00c947
hasanul banna <bannawandoor@gmail.com>
2023-02-22 11:09:53 +0530

    navbar and banner updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      
+     <NavBar/>
     </div>
   )
 }
