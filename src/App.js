dd9a32a5468f6812ea467db743c738c2d98115ff
hasanul banna <bannawandoor@gmail.com>
2023-02-22 11:31:31 +0530

    Rowpost componet created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import './App.css'
+import Banner from './components/Banner/Banner';
 
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Banner/>
     </div>
   )
 }
